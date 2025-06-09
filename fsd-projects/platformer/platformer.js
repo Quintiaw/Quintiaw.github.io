0b01b0cf5423de6793ab5886ed572feaf951e78c
Quintiaw <Quintia.wickem@jpschools.org>
2025-06-09 14:14:30 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -26,21 +26,161 @@ $(function () {
     // ONLY CHANGE BELOW THIS POINT //
     //////////////////////////////////
 
-    // TODO 1 - Enable the Grid
-    // toggleGrid();
 
 
-    // TODO 2 - Create Platforms
+    
+// TODO 1: Enable the grid
+// toggleGrid();
+kaboom({
+  width: 640,
+  height: 480,
+  scale: 2,
+  debug: true,
+  clearColor: [0, 0, 0, 1],
+});
+
+// Load sprites
+loadRoot("https://kaboomjs.com/sprites/");
+loadSprite("bean", "bean.png");
+loadSprite("block", "grass.png");
+
+// Set gravity so the player falls
+setGravity(1200);
+
+// Add the ground platform
+add([
+  rect(width(), 48),
+  pos(0, height() - 48),
+  area(),
+  solid(),
+  color(127, 200, 255),
+]);
+
+// TODO 2 - Create Platforms
+const platforms = [
+  vec2(100, 380),
+  vec2(200, 300),
+  vec2(300, 220),
+  vec2(400, 150),
+];
+
+for (const p of platforms) {
+  add([
+    sprite("block"),
+    pos(p),
+    area(),
+    solid(),
+  ]);
+}
+
+// Add the
+const   add,[
+  sprite("bean"),
+  pos(80, 40),
+  area(),
+  body(),
+
+
+// Player controls
+onKeyDown("left", () => {
+  player.move(-200, 0);
+});
+
+onKeyDown("right", () => {
+  player.move(200, 0);
+});
+
+onKeyPress("space", () => {
+  if (player.isGrounded()) {
+    player.jump(500);
+  }
+});
 
 
 
 
     // TODO 3 - Create Collectables
+    collectables
+const coins = [
+  vec2(120, 340),
+  vec2(220, 260),
+  vec2(320, 180),
+];
+for (const c of coins) {
+  add([
+    sprite("coin"),
+    pos(c),
+    area(),
+    "coin", // 
+  ]);
+}
+// Add the player
+const player = add([
+  sprite("bean"),
+  pos(80, 40),
+  area(),
+  body(),
+]);
+// Player controls
+onKeyDown("left", () => {
+  player.move(-200, 0);
+});
+onKeyDown("right", () => {
+  player.move(200, 0);
+});
+onKeyPress("space", () => {
+  if (player.isGrounded()) {
+    player.jump(500);
+  }
+});
+// TODO: Collect coins when touched
+player.onCollide("coin", (coin) => {
+  destroy(coin);
+  // Optionally play sound or update score
+});
 
 
 
     
     // TODO 4 - Create Cannons
+     loadSprite("cannon", "https://kaboomjs.com/sprites/mark.png");
+
+function spawnBullet(x, y) {
+  const bullet = add([
+    rect(12, 6),
+    pos(x, y),
+    area(),
+    move(LEFT, 300),
+    cleanup(),
+    color(255, 0, 0),
+    "bullet",
+  ]);
+}
+
+function spawnCannon(posX, posY) {
+  add([
+    sprite("cannon"),
+    pos(posX, posY),
+    area(),
+    "cannon",
+  ]);
+
+  loop(2, () => {
+    spawnBullet(posX + 20, posY + 8);
+  });
+}
+
+spawnCannon(500, 400);
+
+player.onCollide("bullet", () => {
+  add([
+    text("Game Over", { size: 32 }),
+    pos(center()),
+    anchor("center"),
+  ]);
+  destroy(player);
+});
+
 
 
     
@@ -52,3 +192,4 @@ $(function () {
 
   registerSetup(setup);
 });
+
